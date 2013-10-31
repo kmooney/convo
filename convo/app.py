e995eebd8bd19fddfaefe2e627038dbcce1c4aed
kmooney <kmooney@gmail.com>
2013-10-30 22:05:13 -0500

    Initial commit

---
+++ convo/app.py
@@ -0,0 +1,24 @@
+import tornado
+from tornado import websocket
+from tornado import httpserver
+
+
+class ChatWebSocketServer(websocket.WebSocketHandler):
+
+    def open(self):
+        print "Websocket Opened"
+
+    def on_message(self, message):
+        self.send(message)
+
+    def on_close(self):
+        print "Socket Closed"
+
+app = tornado.web.Application([
+    (r'/chat', ChatWebSocketServer),
+])
+
+if __name__ == '__main__':
+    http_server = httpserver.HTTPServer(app)
+    http_server.listen(8686)
+    tornado.ioloop.IOLoop.instance().start()
